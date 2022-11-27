e44717df3f77e60ad9795788987c28f119b3e15a
Foundry (vector-client-token) <>
2022-11-27 03:35:58 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -1,10 +1,18 @@
 
 
 @transform_pandas(
-    Output(rid="ri.vector.main.execute.8d04dd48-0628-41a0-b97f-92a65b9fc4ab"),
+    Output(rid="ri.foundry.main.dataset.49397b84-0a01-45a6-991c-b4cbcf33d2ca"),
     person=Input(rid="ri.foundry.main.dataset.39142f4a-4e55-499c-a79a-92b7ba64b13f")
 )
-unnamed <- function(person) {
+# this is a basic plot example
+library(ggplot2)
+
+plot_person <- function(person) {
+    p <- ggplot(person) +
+      geom_histogram(aes(x = year_of_birth))
+
+    plot(p)
     
+    return(NULL)
 }
 
