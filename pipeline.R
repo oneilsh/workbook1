46d60dbfc19fc3d6c5084c4ed935f34d992fd404
Foundry (vector-client-token) <>
2022-11-27 03:40:31 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -4,7 +4,11 @@
     Output(rid="ri.foundry.main.dataset.49397b84-0a01-45a6-991c-b4cbcf33d2ca"),
     person=Input(rid="ri.foundry.main.dataset.39142f4a-4e55-499c-a79a-92b7ba64b13f")
 )
-# this is a basic plot example
+#* plot_person:
+#*   inputs:
+#*     - person
+#*   code: |
+#* this is a basic plot example
 library(ggplot2)
 
 plot_person <- function(person) {
--- pipeline.sql
+++ pipeline.sql
@@ -4,7 +4,11 @@
     Output(rid="ri.vector.main.execute.adfead7f-5801-4c46-b48e-d377c82f12ce"),
     concept_set_members=Input(rid="ri.foundry.main.dataset.e670c5ad-42ca-46a2-ae55-e917e3e161b6")
 )
-
+--* input_concept_set_members:
+--*   ext: sql
+--*   inputs:
+--*     - concept_set_members
+--*   code: |
 SELECT *
 FROM concept_set_members
 
@@ -12,6 +16,11 @@ FROM concept_set_members
     Output(rid="ri.vector.main.execute.db839486-9cda-4e2b-90f5-c0711f0dbe55"),
     input_concept_set_members=Input(rid="ri.vector.main.execute.adfead7f-5801-4c46-b48e-d377c82f12ce")
 )
+--* pneumonia_concept_set:
+--*   ext: sql
+--*   inputs:
+--*     - input_concept_set_members
+--*   code: |
 SELECT concept_id 
 FROM input_concept_set_members
 WHERE codeset_id = 980588199 and is_most_recent_version = true
@@ -21,6 +30,12 @@ WHERE codeset_id = 980588199 and is_most_recent_version = true
     condition_era=Input(rid="ri.foundry.main.dataset.a00c67ed-f30b-4deb-9f6f-a7febafd3f8f"),
     pneumonia_concept_set=Input(rid="ri.vector.main.execute.db839486-9cda-4e2b-90f5-c0711f0dbe55")
 )
+--* pneumonia_conditions:
+--*   ext: sql
+--*   inputs:
+--*     - pneumonia_concept_set
+--*     - condition_era
+--*   code: |
 SELECT *
 FROM condition_era
 INNER JOIN pneumonia_concept_set ON pneumonia_concept_set.concept_id = condition_era.condition_concept_id
