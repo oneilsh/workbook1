a3b41b77615b252beba07fee5f22a94e7f556f19
Foundry (vector-client-token) <>
2022-11-27 03:34:00 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -1,2 +1,10 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.8d04dd48-0628-41a0-b97f-92a65b9fc4ab"),
+    person=Input(rid="ri.foundry.main.dataset.39142f4a-4e55-499c-a79a-92b7ba64b13f")
+)
+unnamed <- function(person) {
+    
+}
+
--- pipeline.sql
+++ pipeline.sql
@@ -17,9 +17,11 @@ FROM input_concept_set_members
 WHERE codeset_id = 980588199 and is_most_recent_version = true
 
 @transform_pandas(
-    Output(rid="ri.vector.main.execute.8bd26a33-fce7-4a8d-b34c-20ebdad166bc"),
+    Output(rid="ri.foundry.main.dataset.130ca484-5580-48a9-9480-686c6ea1824a"),
+    condition_era=Input(rid="ri.foundry.main.dataset.a00c67ed-f30b-4deb-9f6f-a7febafd3f8f"),
     pneumonia_concept_set=Input(rid="ri.vector.main.execute.db839486-9cda-4e2b-90f5-c0711f0dbe55")
 )
 SELECT *
-FROM pneumonia_concept_set
+FROM condition_era
+INNER JOIN pneumonia_concept_set ON pneumonia_concept_set.concept_id = condition_era.condition_concept_id
 
